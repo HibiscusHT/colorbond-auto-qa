cd225154ea4ce7f9a94d7790ba82ad932a9c83fa
ASTOMO PANCORO PUTRO <93188135+HibiscusHT@users.noreply.github.com>
2025-03-06 04:14:14 +0000

    successfully test search fitur

--- cypress/e2e/colorbond.cy.js
+++ cypress/e2e/colorbond.cy.js
@@ -4,8 +4,27 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   
   describe('colorbond auto qa', () => {
 
-    it('first test',()=>{
-        cy.task('writeLine','first test')
+    let myUrl = Cypress.env('base_url')
+    let newUrl = ''
+      
+    before(() => {
+      cy.task('writeLine',`automation test by ${Cypress.env('developer')} version: ${Cypress.env('version')}`)
+      cy.visit(myUrl)
+      cy.screenshot()
+    })
+
+    it('search and open article entitled award',()=>{
+        cy.task('writeLine','open search input box')
+        cy.get('div.hidden.flex-wrap.justify-end.items-center > div.block.global-search').click()
+        cy.screenshot()
+        cy.task('writeLine','input keyword')
+        cy.get('body > div.min-h-screen > div.top-0.left-0.w-full.h-full.overlay-black.global-search-overlay.z-50 > div.block.relative.w-full.h-full > form > div > input').type('award {enter}')
+        cy.url().should('include', '/search')
+        cy.screenshot()
+        cy.task('writeLine','click article entitle shinta prima')
+        cy.get('body > div.min-h-screen > div.flex-col.w-full > main > div.flex.flex-wrap.w-full.my-12.py-12 > div > div > div > a:nth-child(3)').click()
+        cy.url().should('include','/inspirations/shinta-prima-feedmill-nganjuk')
+        cy.screenshot()
     })
 
     it('second test',()=>{
